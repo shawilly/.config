f3cec3ab977ec22ac17e07352cd465147c8d98d6
Shane Williams <shanebarrywilliams@gmail.com>
2024-04-17 09:25:35 +0100

    Remove ui from nvim config
    Nerd fonts and symbols are natively supported by wezterm

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,13 @@
+--Essentials
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
+
+-- NOTE: For more options, you can see `:help option-list`
 vim.o.clipboard = 'unnamedplus'
 vim.opt.number = true
 vim.opt.relativenumber = true
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+vim.opt.mouse = 'a' --mouse available if you need it, you shouldn't ;)
 vim.opt.showmode = true
 vim.opt.breakindent = true
 vim.opt.undofile = true
@@ -37,33 +37,25 @@ vim.diagnostic.config {
   update_in_insert = false, -- default to false
   severity_sort = false, -- default to false
 }
+
+-- Keymaps
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
--- NOTE: Disable arrow keys in neovim so you actually learn
+
+-- NOTE: trying to teach you not to suck so much
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h ya idiot"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l ya idiot"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k ya idiot"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j ya idiot"<CR>')
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
@@ -78,7 +70,6 @@ vim.lsp.buf.format {
   end,
 }
 
--- Install lazyvim
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
