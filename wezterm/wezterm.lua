3390d37a82bd5b3daaffd5465ce175ec0d40cee3
Shane Williams <shane.williams@resourcekraft.com>
2024-05-03 10:11:02 +0100

    Add zen and font size changer

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -38,13 +38,12 @@ config.window_background_gradient = {
 	blend = "Rgb",
 }
 
--- font_size
+-- font
 config.font = wezterm.font({
 	family = "RobotoMono Nerd Font",
 	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 	weight = "Bold",
 })
-config.font_size = 17
 config.bold_brightens_ansi_colors = true
 
 -- tab bar
@@ -170,6 +169,29 @@ wezterm.on("update-right-status", function(window, pane)
 	window:set_right_status(wezterm.format({
 		{ Text = cwd .. "    " .. battery_status .. " " .. date },
 	}))
+end)
+
+wezterm.on("user-var-changed", function(window, pane, name, value)
+	local overrides = window:get_config_overrides() or {}
+	if name == "ZEN_MODE" then
+		local incremental = value:find("+")
+		local number_value = tonumber(value)
+		if incremental ~= nil then
+			while number_value > 0 do
+				window:perform_action(wezterm.action.IncreaseFontSize, pane)
+				number_value = number_value - 1
+			end
+			overrides.enable_tab_bar = false
+		elseif number_value < 0 then
+			window:perform_action(wezterm.action.ResetFontSize, pane)
+			overrides.font_size = nil
+			overrides.enable_tab_bar = true
+		else
+			overrides.font_size = number_value
+			overrides.enable_tab_bar = false
+		end
+	end
+	window:set_config_overrides(overrides)
 end)
 
 -- mouse (yuck)
