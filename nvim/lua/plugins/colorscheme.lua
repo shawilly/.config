56602a0726161e7919c577e08a1c7ddf377262b9
Shane Williams <shane.williams@resourcekraft.com>
2024-05-15 14:36:37 +0100

    Colorschme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -33,6 +33,7 @@ local _ponokai = {
 	"shawilly/ponokai",
 	priority = 1000,
 	config = function()
+		vim.g.ponokai_transparent_background = "1"
 		vim.g.ponokai_enable_italic = "1"
 		vim.g.ponokai_style = "kitty"
 		vim.cmd.colorscheme("ponokai")
