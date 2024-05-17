913edb5aae74f16710feddd754788ca70d5b1037
Shane Williams <shane.williams@resourcekraft.com>
2024-05-17 12:20:41 +0100

    Git alias and ck alias

---
+++ fish/functions/mck.fish
@@ -0,0 +1,4 @@
+function mck --wraps='ssh -v -i "~/.ssh/ChargeKomplete.pem" -L 27019:docdb-2023-07-04-12-47-26.cquini0d1ggh.eu-west-1.docdb.amazonaws.com:27017 ubuntu@52.215.115.238 -N' --wraps='ssh -v -i "~/.ssh/CKKEy.pem" -L 27019:docdb-2023-07-04-12-47-26.cquini0d1ggh.eu-west-1.docdb.amazonaws.com:27017 ubuntu@52.215.115.238 -N' --description 'alias mck=ssh -v -i "~/.ssh/CKKEy.pem" -L 27019:docdb-2023-07-04-12-47-26.cquini0d1ggh.eu-west-1.docdb.amazonaws.com:27017 ubuntu@52.215.115.238 -N'
+  ssh -v -i "~/.ssh/CKKEy.pem" -L 27019:docdb-2023-07-04-12-47-26.cquini0d1ggh.eu-west-1.docdb.amazonaws.com:27017 ubuntu@52.215.115.238 -N $argv
+        
+end
