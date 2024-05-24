55086fc6ccb96138f813bcc42fd06d1cb3d73ee0
Shane Williams <shane.williams@resourcekraft.com>
2024-05-24 20:15:30 +0100

    Refactor wezterm
    Remove moon, consolidate icons, remove unused variables and functions

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,29 +4,6 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 local nf = wezterm.nerdfonts
 
--- Functions
-local get_last_folder_segment = function(cwd)
-	if cwd == nil then
-		return "N/A" -- or some default value you prefer
-	end
-
-	-- Strip off 'file:///' if present
-	local pathStripped = cwd:match("^file:///(.+)") or cwd
-	-- Normalize backslashes to slashes for Windows paths
-	pathStripped = pathStripped:gsub("\\", "/")
-	-- Split the path by '/'
-	local path = {}
-	for segment in string.gmatch(pathStripped, "[^/]+") do
-		table.insert(path, segment)
-	end
-	return path[#path] -- returns the last segment
-end
-
-local function get_current_working_dir(tab)
-	local current_dir = tab.active_pane.current_working_dir or ""
-	return get_last_folder_segment(current_dir)
-end
-
 local process_icons = {
 	["docker"] = nf.linux_docker,
 	["docker-compose"] = nf.linux_docker,
@@ -55,17 +32,14 @@ local process_icons = {
 	["node"] = nf.dev_nodejs_small,
 	["dotnet"] = nf.md_language_csharp,
 	["fish"] = nf.md_fish .. nf.fa_terminal,
+	["mongo"] = nf.dev_mongodb,
+	["ssh"] = nf.md_ssh,
+	["python"] = nf.dev_python,
+	["admin"] = nf.oct_shield,
+	["lambda"] = nf.md_lambda,
+	["left_arrow"] = nf.md_arrow_left,
+	["right_arrow"] = nf.md_arrow_right,
 }
-
-local function get_process(tab)
-	local process_name = tab.active_pane.foreground_process_name:match("([^/\\]+)%.exe$")
-		or tab.active_pane.foreground_process_name:match("([^/\\]+)$")
-
-	-- local icon = process_icons[process_name] or string.format('[%s]', process_name)
-	local icon = process_icons[process_name] or nf.seti_checkbox_unchecked
-
-	return icon
-end
 
 -- This table will hold the configuration.
 local config = {}
@@ -115,6 +89,7 @@ config.color_scheme = "OLEDppuccin"
 config.font = wezterm.font({
 	family = "JetBrainsMono Nerd Font",
 	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+	size = 16.0,
 })
 config.font_size = 8.5
 config.bold_brightens_ansi_colors = true
@@ -128,34 +103,6 @@ config.status_update_interval = 1000
 local function basename(s)
 	return string.gsub(s, "(.*[/\\])(.*)", "%2")
 end
-
-local SOLID_LEFT_ARROW = utf8.char(0xe0ba)
-local SOLID_LEFT_MOST = utf8.char(0x2588)
-local SOLID_RIGHT_ARROW = utf8.char(0xe0bc)
-
-local ADMIN_ICON = utf8.char(0xf49c)
-
-local CMD_ICON = utf8.char(0xe62a)
-local NU_ICON = utf8.char(0xe7a8)
-local PS_ICON = utf8.char(0xe70f)
-local ELV_ICON = utf8.char(0xfc6f)
-local WSL_ICON = utf8.char(0xf83c)
-local YORI_ICON = utf8.char(0xf1d4)
-local NYA_ICON = utf8.char(0xf61a)
-
-local VIM_ICON = utf8.char(0xe62b)
-local PAGER_ICON = utf8.char(0xf718)
-local FUZZY_ICON = utf8.char(0xf0b0)
-local HOURGLASS_ICON = utf8.char(0xf252)
-local SUNGLASS_ICON = utf8.char(0xf9df)
-
-local PYTHON_ICON = utf8.char(0xf820)
-local NODE_ICON = utf8.char(0xe74e)
-local DENO_ICON = utf8.char(0xe628)
-local LAMBDA_ICON = utf8.char(0xfb26)
-local MONGO_ICON = nf.dev_mongodb
-local FISH_ICON = nf.md_fish .. nf.fa_terminal
-local SSH_ICON = nf.md_ssh
 
 local SUP_IDX = {
 	"¹",
@@ -202,60 +149,24 @@ local SUB_IDX = {
 	"₂₀",
 }
 
-wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+wezterm.on("format-tab-title", function(tab, _tabs, _panes, _config, _hover, max_width)
 	local process_name = tab.active_pane.foreground_process_name
 	local pane_title = tab.active_pane.title
 	local exec_name = basename(process_name):gsub("%.exe$", "")
-	local title_with_icon
+	local title_with_icon = process_icons[exec_name] or nf.fa_terminal
 
-	if exec_name == "nu" then
-		title_with_icon = NU_ICON .. " NuShell"
-	elseif exec_name == "pwsh" then
-		title_with_icon = PS_ICON .. " PS"
-	elseif exec_name == "cmd" then
-		title_with_icon = CMD_ICON .. " CMD"
-	elseif exec_name == "elvish" then
-		title_with_icon = ELV_ICON .. " Elvish"
-	elseif exec_name == "wsl" or exec_name == "wslhost" then
-		title_with_icon = WSL_ICON .. " WSL"
-	elseif exec_name == "nyagos" then
-		title_with_icon = NYA_ICON .. " " .. pane_title:gsub(".*: (.+) %- .+", "%1")
-	elseif exec_name == "yori" then
-		title_with_icon = YORI_ICON .. " " .. pane_title:gsub(" %- Yori", "")
-	elseif exec_name == "nvim" then
-		title_with_icon = VIM_ICON .. " " .. pane_title:gsub("^(%S+)%s+(%d+/%d+) %- nvim", " %2 %1")
-	elseif exec_name == "bat" or exec_name == "less" or exec_name == "moar" then
-		title_with_icon = PAGER_ICON .. " " .. exec_name:upper()
-	elseif exec_name == "fzf" or exec_name == "hs" or exec_name == "peco" then
-		title_with_icon = FUZZY_ICON .. " " .. exec_name:upper()
-	elseif exec_name == "btm" or exec_name == "ntop" then
-		title_with_icon = SUNGLASS_ICON .. " " .. exec_name:upper()
-	elseif exec_name == "python" or exec_name == "hiss" then
-		title_with_icon = PYTHON_ICON .. " " .. exec_name
-	elseif exec_name == "node" then
-		title_with_icon = NODE_ICON .. " " .. exec_name:upper()
-	elseif exec_name == "deno" then
-		title_with_icon = DENO_ICON .. " " .. exec_name:upper()
+	if exec_name == "nvim" then
+		title_with_icon = title_with_icon .. " " .. pane_title:gsub("^(%S+)%s+(%d+/%d+) %- nvim", " %2 %1")
 	elseif exec_name == "bb" or exec_name == "cmd-clj" or exec_name == "janet" or exec_name == "hy" then
-		title_with_icon = LAMBDA_ICON .. " " .. exec_name:gsub("bb", "Babashka"):gsub("cmd%-clj", "Clojure")
-	elseif exec_name == "mongo" then
-		title_with_icon = MONGO_ICON .. " " .. exec_name
-	elseif exec_name == "fish" then
-		title_with_icon = FISH_ICON .. " " .. pane_title:gsub("~/", "")
-	elseif exec_name == "ssh" then
-		title_with_icon = SSH_ICON
+		title_with_icon = title_with_icon .. " " .. exec_name:gsub("bb", "Babashka"):gsub("cmd%-clj", "Clojure")
 	elseif exec_name == "bash" then
-		title_with_icon = process_icons[exec_name] .. " " .. pane_title:gsub("~/", "")
-	else
-		title_with_icon = HOURGLASS_ICON .. " " .. exec_name
+		title_with_icon = title_with_icon .. " " .. pane_title:gsub("~/", "")
 	end
+
 	if pane_title:match("^Administrator: ") then
 		title_with_icon = title_with_icon .. "  " .. ADMIN_ICON
 	end
-	local left_arrow = SOLID_LEFT_ARROW
-	if tab.tab_index == 0 then
-		left_arrow = SOLID_LEFT_MOST
-	end
+
 	local id = SUB_IDX[tab.tab_index + 1]
 	local pid = SUP_IDX[tab.active_pane.pane_index + 1]
 	local title = " " .. wezterm.truncate_right(title_with_icon, max_width - 6) .. " "
@@ -273,7 +184,6 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
 end)
 
 wezterm.on("update-status", function(window, pane)
-	local moon = nf.weather_moon_full
 	local battery = nf.md_battery_heart
 	local charge_color = "lightgreen"
 	local charge_percent = ""
@@ -283,23 +193,18 @@ wezterm.on("update-status", function(window, pane)
 		charge_percent = string.format("%.0f%%", b.state_of_charge * 100)
 
 		if b.state_of_charge < 0.2 then
-			moon = nf.weather_moon_waning_crescent_2
 			battery = nf.md_battery_alert_variant_outline
 			charge_color = "#F3B0A7"
 		elseif b.state_of_charge < 0.4 then
-			moon = nf.weather_moon_waning_crescent_1
 			battery = nf.md_battery_40
 			charge_color = "#FFDAB9"
 		elseif b.state_of_charge < 0.6 then
-			moon = nf.weather_moon_waning_gibbous_2
 			battery = nf.md_battery_60
 			charge_color = "#F5F5B8"
 		elseif b.state_of_charge < 0.8 then
 			charge_color = "#B8E6C9"
-			moon = nf.weather_moon_waning_gibbous_1
 			battery = nf.md_battery_80
 		else
-			moon = nf.weather_moon_full
 			battery = nf.md_battery_heart
 		end
 
