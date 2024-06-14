53f7016e0eb42ae8a65ef3c8043b25e1ed9ffc26
Shane Williams <shane.williams@resourcekraft.com>
2024-06-14 08:49:31 +0100

    Secrets ignore for wezterm, cn function in fish, nerd font starship

--- fish/config.fish
+++ fish/config.fish
@@ -1,22 +1,20 @@
 # ~/.config/fish/config.fish
 
-# Kanagawa Fish shell theme
-# A template was taken and modified from Tokyonight:
-# https://github.com/folke/tokyonight.nvim/blob/main/extras/fish_tokyonight_night.fish
-set -l foreground DCD7BA normal
-set -l selection 2D4F67 brcyan
-set -l comment 727169 brblack
-set -l red C34043 red
-set -l orange FF9E64 brred
-set -l yellow C0A36E yellow
-set -l green 76946A green
-set -l purple 957FB8 magenta
-set -l cyan 7AA89F cyan
-set -l pink D27E99 brmagenta
+# Color Definitions
+set -l foreground "#f2f2f3"
+set -l selection "#363a4e"
+set -l comment "#9da1af"
+set -l red "#ff667a"
+set -l orange "#f3bb9a"
+set -l yellow "#f8e7b0"
+set -l green "#91cf6a"
+set -l purple "#bdb2ff"
+set -l cyan "#98d4e7"
+set -l pink "#865aff"
 
 # Syntax Highlighting Colors
 set -g fish_color_normal $foreground
-set -g fish_color_command $cyan
+set -g fish_color_command $cyan --bold
 set -g fish_color_keyword $pink
 set -g fish_color_quote $yellow
 set -g fish_color_redirection $foreground
@@ -24,11 +22,19 @@ set -g fish_color_end $orange
 set -g fish_color_error $red
 set -g fish_color_param $purple
 set -g fish_color_comment $comment
-set -g fish_color_selection --background=$selection
+set -g fish_color_selection --background=$selection --bold
 set -g fish_color_search_match --background=$selection
 set -g fish_color_operator $green
-set -g fish_color_escape $pink
+set -g fish_color_escape $pink --bold
 set -g fish_color_autosuggestion $comment
+set -g fish_color_cancel -r
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_match --background=brblue
+set -g fish_color_user brgreen
+set -g fish_color_valid_path --underline
 
 # Completion Pager Colors
 set -g fish_pager_color_progress $comment
@@ -54,4 +60,14 @@ end
 # Setting Android SDK environment variables
 set -gx ANDROID_HOME $HOME/Library/Android/sdk
 set -gx PATH $ANDROID_HOME/emulator $ANDROID_HOME/platform-tools $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $PATH
+
+function starship_transient_prompt_func
+    starship module character
+end
+
+function starship_transient_rprompt_func
+    starship module time
+end
+
 starship init fish | source
+enable_transience
---
+++ fish/functions/cn.fish
@@ -0,0 +1,4 @@
+function cn --wraps='cnf && n' --description 'alias cn=cnf && n'
+  cnf && n $argv
+        
+end
